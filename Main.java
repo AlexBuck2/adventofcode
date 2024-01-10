48b12f5624af599428b636db06bced2bc7aa4dfa
AlexBuck2 <145092421+AlexBuck2@users.noreply.github.com>
2024-01-09 22:48:41 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) {
@@ -105,8 +107,9 @@ public class Main {
                 "Game 98: 2 blue, 7 green, 1 red; 9 green, 10 red, 5 blue; 13 blue, 10 red, 8 green; 8 green, 11 red, 12 blue; 5 blue, 4 green, 2 red\n" +
                 "Game 99: 2 blue, 13 green; 1 blue; 1 red, 2 blue, 2 green; 1 red, 1 blue\n" +
                 "Game 100: 1 red, 14 green; 17 green, 12 red; 3 green, 7 red, 3 blue; 4 green, 13 red, 3 blue; 5 green, 11 red, 5 blue";
-        ArrayList<String> test = new ArrayList<String>(arrayMaker(input));
 
+        ArrayList<String> test = new ArrayList<String>(arrayMaker(input));
+        System.out.println("day 2.2: " + day22(test));
         //  System.out.println("\t" + valueFinder(test));
         // System.out.println(input);
 
@@ -116,16 +119,32 @@ public class Main {
          */
         //int result2 = calc(test);
         // System.out.println(letterValueFinder(test));
-       int result = iDCalculator(test);
-       System.out.println(result);
+        // int result = iDCalculator(test);
+        //  System.out.println(result);
         /**
-        String array2D = "467..114..\n" +
-                "...*......\n" +
-                "..35..633.";
-        arrayMaker2D(array2D);
-        char[][] twoDArray = arrayMaker2(array2D);
-        printTable1(twoDArray);
+         String array2D = "467..114..\n" +
+         "...*......\n" +
+         "..35..633.";
+         arrayMaker2D(array2D);
+         char[][] twoDArray = arrayMaker2(array2D);
+         printTable1(twoDArray);
+
+         String engineSchematicString = "467..114..\n" +
+         "...*......\n" +
+         "..35..633.\n" +
+         "......#...\n" +
+         "617*......\n" +
+         ".....+.58.\n" +
+         "..592.....\n" +
+         "......755.\n" +
+         "...$.*....\n" +
+         ".664.598..";
+         //  char[][] engineSchematic = arrayMaker31(engineSchematicString);
+
+         int sum = engine(engineSchematicString);
+         System.out.println("Sum of part numbers: " + sum);
          */
+
     }
 
     public static ArrayList<String> arrayMaker(String starter) {
@@ -178,7 +197,7 @@ public class Main {
     }
 
 
-            public static void printTable(ArrayList<ArrayList<Character>> table) {
+    public static void printTable(ArrayList<ArrayList<Character>> table) {
         for (ArrayList<Character> row : table) {
             for (Character value : row) {
                 System.out.print(value + "\t");
@@ -314,25 +333,25 @@ public class Main {
     public static int iDCalculator(ArrayList<String> array) {
         int total = 0;
         for (int i = 0; i < array.size(); i++) {
-           // System.out.println(array.get(i));
+            // System.out.println(array.get(i));
             String temp = "";
             String idString = "";
             boolean checker = true;
             for (int n = array.get(i).indexOf(':'); n < array.get(i).length(); n++) {
                 if (array.get(i).charAt(n) == '0' || array.get(i).charAt(n) == '1' || array.get(i).charAt(n) == '2' || array.get(i).charAt(n) == '3' || array.get(i).charAt(n) == '4' || array.get(i).charAt(n) == '5' || array.get(i).charAt(n) == '6' || array.get(i).charAt(n) == '7' || array.get(i).charAt(n) == '8' || array.get(i).charAt(n) == '9') {
                     temp += array.get(i).charAt(n);
-                   // System.out.println(array.get(i).charAt(n));
+                    // System.out.println(array.get(i).charAt(n));
                 }
                 if (array.get(i).charAt(n) == ' ' && Character.isDigit(array.get(i).charAt(n - 1)) && Character.isLetter(array.get(i).charAt(n + 1))) {
                     int tempNumber = Integer.parseInt(temp);
-                   // System.out.println("test" + tempNumber);
+                    // System.out.println("test" + tempNumber);
                     // Adjust the conditions based on your intended logic
                     if ((array.get(i).charAt(n + 1) == 'r' && tempNumber <= 12) || (array.get(i).charAt(n + 1) == 'g' && tempNumber <= 13) || (array.get(i).charAt(n + 1) == 'b' && tempNumber <= 14)) {
-                       // System.out.println("test 2" + temp);
+                        // System.out.println("test 2" + temp);
                         temp = "";
                         System.out.println("test 3" + "." + temp);
                     } else {
-                       // System.out.println("test 8" + temp + " " + tempNumber);
+                        // System.out.println("test 8" + temp + " " + tempNumber);
                         temp = "";
                         checker = false;
                         break;
@@ -406,6 +425,50 @@ public class Main {
         return total;
     }
 
+    public static int day22(ArrayList<String> array){
+        int total = 0;
+     for(int i = 0; i < array.size(); i++) {
+         int greatestBlue = 0;
+         int greatestRed = 0;
+         int greatestGreen = 0;
+
+         int toBeAdded = 1;
+
+
+         Pattern blue = Pattern.compile("(\\d+)\\s+blue");
+         Pattern red = Pattern.compile("(\\d+)\\s+red");
+         Pattern green = Pattern.compile("(\\d+)\\s+green");
+
+         Matcher blueMatcher = blue.matcher(array.get(i));
+         Matcher redMatcher = red.matcher(array.get(i));
+         Matcher greenMatcher = green.matcher(array.get(i));
+
+         while(blueMatcher.find()){
+             int curr = Integer.parseInt(blueMatcher.group(1));
+             if(curr > greatestBlue) {
+                 greatestBlue = curr;
+             }
+         }
+         while(redMatcher.find()){
+             int curr = Integer.parseInt(redMatcher.group(1));
+             if(curr > greatestRed) {
+                 greatestRed = curr;
+             }
+         }
+         while(greenMatcher.find()){
+             int curr = Integer.parseInt(greenMatcher.group(1));
+             if(curr > greatestGreen) {
+                 greatestGreen = curr;
+             }
+         }
+
+         toBeAdded *= greatestBlue * greatestRed * greatestGreen;
+
+        total += toBeAdded;
+     }
+        return total;
+    }
+
     public static int day3(char[][] array) {
         int total = 0;
         for (int row = 0; row < array.length; row++) {
@@ -416,44 +479,299 @@ public class Main {
                     temp += array[row][col];
                 }
                 if (isNum == false && !temp.isEmpty()) {
-                    for (int q = -1; q < temp.length() + 1; q++) {
-                        if(row > 0 && col < array[row].length -1 ){
+
+
+                    if (row > 0 && col > 0 && col < array[row].length - 1 && row < array.length) {
+                        for(int q = -1; q < temp.length() +1; q++){
                             if (array[row - 1][col + q] != '.' && !Character.isDigit(array[row - 1][col + q]) || array[row + 1][col + q] != '.' && !Character.isDigit(array[row + 1][col + q])) {
-
+                                int tempNumber = Integer.parseInt(temp);
+                                total+= tempNumber;
+                                break;
                             }
-                        } else if (row < 1 && col < array[row].length -1) {
-                            if(array[row + 1][col + q] != '.' && !Character.isDigit(array[row + 1][col + q])  ){
-
-                            }
-
-                        } else if(row > 0 && col > array[row].length -2){
-
-                        } else {
-
                         }
-
-
-                        /**if(array[row][col] != '.' && Character.isDigit(array[row][col])){
-                         try {
-                         if(Character.isDigit(array[row-1][col-1])){
-                         numberFinderday3(array[row-1],col-1);
-                         //replace commas
-                         //repeat for all values
-                         }
-                         } catch (ArrayIndexOutOfBoundsException e) {
-                         System.out.println("Caught an exception: " + e.getMessage());
-                         }
-                         }
-                         */
+                        // Case 1
+                    } else if (row <= 0 && col > 0 && col < array[row].length - 1 && row < array.length) {
+                        // Case 2
+                    } else if (row <= 0 && col >= array[row].length - 1 && row < array.length && col > 0) {
+                        // Case 3
+                    } else if (row <= 0 && col >= array[row].length - 1 && row >= array.length && col > 0) {
+                        // Case 4
+                    } else if (row <= 0 && col >= array[row].length - 1 && row >= array.length && col <= 0) {
+                        // Case 5
+                    } else if (row > 0 && col >= array[row].length - 1 && row >= array.length && col <= 0) {
+                        // Case 6
+                    } else if (row > 0 && col < array[row].length - 1 && row >= array.length && col <= 0) {
+                        // Case 7
+                    } else if (row > 0 && col < array[row].length - 1 && row < array.length && col <= 0) {
+                        // Case 8
+                    } else if (row <= 0 && col < array[row].length - 1 && row >= array.length && col <= 0) {
+                        // Case 9
+                    } else if (row > 0 && col > 0 && col >= array[row].length - 1 && row < array.length) {
+                        // Case 10
+                    } else if (row <= 0 && col > 0 && col >= array[row].length - 1 && row < array.length) {
+                        // Case 11
+                    } else if (row > 0 && col > 0 && col < array[row].length - 1 && row >= array.length) {
+                        // Case 12
+                    } else if (row > 0 && col <= 0 && row < array.length) {
+                        // Case 13
+                    } else if (row <= 0 && col <= 0) {
+                        // Case 14
+                    } else if (row <= 0 && col > 0 && col < array[row].length - 1 && row >= array.length) {
+                        // Case 15
+                    } else if (row > 0 && col <= 0 && row >= array.length) {
+                        // Case 16
                     }
+
+
+
+
+                    /**if(array[row][col] != '.' && Character.isDigit(array[row][col])){
+                     try {
+                     if(Character.isDigit(array[row-1][col-1])){
+                     numberFinderday3(array[row-1],col-1);
+                     //replace commas
+                     //repeat for all values
+                     }
+                     } catch (ArrayIndexOutOfBoundsException e) {
+                     System.out.println("Caught an exception: " + e.getMessage());
+                     }
+                     }
+                     */
+
                 }
                 return 1;
             }
         }
+        return 1;
     }
     public static String numberFinderday3(char [] array, int index){
 
 
         return "";
+    }
+
+
+    public static int engine(String input) {
+        char[][] engine = arrayMaker31(input);
+        boolean keepNumber = false;
+        String number = "";
+        int sum = 0;
+
+        for (int i = 1; i < engine.length - 1; i++) {
+            for (int j = 1; j < engine[i].length - 1; j++) {
+                if (engine[i][j] >= '0' && engine[i][j] <= '9') {
+                    number += engine[i][j];
+                    if (checkAdjustments(engine, i, j)) {
+                        keepNumber = true;
+                    }
+                } else {
+                    if (keepNumber && !number.isEmpty()) {
+                        sum += Integer.parseInt(number);
+                    }
+
+                    keepNumber = false;
+                    number = "";
+                }
+            }
+
+            if (keepNumber && !number.isEmpty()) {
+                sum += Integer.parseInt(number);
+            }
+
+            keepNumber = false;
+        }
+
+        return sum;
+    }
+
+    private static boolean checkAdjustments(char[][] engine, int i, int j) {
+        return engine[i + 1][j] != '.' || engine[i - 1][j] != '.' || engine[i + 1][j + 1] != '.' || engine[i + 1][j - 1] != '.' || engine[i - 1][j + 1] != '.' || engine[i - 1][j - 1] != '.' || !isDigitOrDot(engine[i][j + 1]) || !isDigitOrDot(engine[i][j - 1]);
+    }
+
+    private static boolean isDigitOrDot(char c) {
+        return c >= '0' && c <= '9' || c == '.';
+    }
+
+    private static char[][] arrayMaker31(String s) {
+        String[] split = s.split("\n");
+        char[][] array = new char[split.length][];
+
+        for (int i = 0; i < split.length; i++) {
+            array[i] = split[i].toCharArray();
+        }
+
+        return array;
+    }
+    public static HashMap<String, String> hashMapMaker(ArrayList<String> array){
+        HashMap<String, String> map = new HashMap<>();
+        for(int i = 0; i < array.size(); i++){
+            map.put(array.get(i).substring(0, array.get(i).indexOf(":")), array.get(i).substring(array.get(i).indexOf(":"), array.get(i).length()));
+        }
+        return map;
+    }
+
+    //HashMap<String, String> map
+    public static int day42(ArrayList<String> array){
+        //   for(Map.Entry<String, String> entry : map.entrySet()){
+        // }
+
+        ArrayList<ArrayList<String>> index = new ArrayList<>();
+        for(int i = 0; i < array.size(); i++){
+            ArrayList<String> inner = new ArrayList<>();
+            index.add(inner);
+        }
+        for(int i = 0; i < array.size(); i++){
+            index.get(i).add(array.get(i));
+        }
+        int counter = 0;
+        for(int i = 0; i < index.size(); i++){
+            System.out.println(i);
+            for(int n = 0; n < index.get(i).size(); n++){
+                String compact = index.get(i).get(n).substring(index.get(i).get(n).indexOf(':') + 1);
+                String [] split = compact.split("\\|");
+                Pattern pattern = Pattern.compile("\\d+");
+
+                Matcher matcher = pattern.matcher(split[0]);
+                Matcher matcher1 = pattern.matcher(split[1]);
+
+                ArrayList<Integer> myNumbers = new ArrayList<>();
+                ArrayList<Integer> winningNumbers = new ArrayList<>();
+
+                while (matcher.find()) {
+                    int number = Integer.parseInt(matcher.group());
+                    myNumbers.add(number);
+                }
+
+                while (matcher1.find()) {
+                    int number = Integer.parseInt(matcher1.group());
+                    winningNumbers.add(number);
+                }
+
+                HashSet<Integer> overlapping = new HashSet<>(myNumbers);
+                overlapping.retainAll(winningNumbers);
+
+                int count = overlapping.size();
+
+                if(count > 0){
+                    for(int z = 1; z <= count; z++){
+                        String duplicate = index.get(i+z).get(n);
+                        index.get(i+z).add(duplicate);
+                    }
+                }
+            }
+
+
+        }
+        for(int w = 0; w < index.size(); w++){
+            counter += index.get(w).size();
+        }
+
+        return counter;
+    }
+    public static int day4(ArrayList<String> array){
+
+        int total = 0;
+        for (int i = 0; i < array.size(); i++) {
+            String compact = array.get(i).substring(array.get(i).indexOf(':') + 1);
+            String[] split = compact.split("\\|");
+            Pattern pattern = Pattern.compile("\\d+");
+
+            Matcher matcher = pattern.matcher(split[0]);
+            Matcher matcher1 = pattern.matcher(split[1]);
+
+            ArrayList<Integer> myNumbers = new ArrayList<>();
+            ArrayList<Integer> winningNumbers = new ArrayList<>();
+
+            while (matcher.find()) {
+                int number = Integer.parseInt(matcher.group());
+                myNumbers.add(number);
+            }
+
+            while (matcher1.find()) {
+                int number = Integer.parseInt(matcher1.group());
+                winningNumbers.add(number);
+            }
+
+            HashSet<Integer> overlapping = new HashSet<>(myNumbers);
+            overlapping.retainAll(winningNumbers);
+
+            int doubler = overlapping.size();
+
+            total += Math.pow(2, overlapping.size()-1);
+        }
+
+        return total;
+    }
+    public static int day6(ArrayList<String> array){
+        ArrayList<Integer>  time = new ArrayList<>();
+        ArrayList<Integer> distance = new ArrayList<>();
+        int total = 1;
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(array.get(0));
+        Matcher matcher1 = pattern.matcher(array.get(1));
+        while(matcher.find()){
+            int number = Integer.parseInt(matcher.group());
+            time.add(number);
+        }
+        while(matcher1.find()){
+            int number = Integer.parseInt(matcher1.group());
+            distance.add(number);
+        }
+        for(int n = 0; n < time.size(); n ++){
+            int time1 = time.get(n);
+            int distance1 = distance.get(n);
+            int roundScore = 0;
+            for(int z = 0; z < time1; z++){
+                int charge = z;
+                int toTravel = time1-z;
+                if((charge * toTravel) > distance1){
+                    roundScore += 1;
+                }
+            }
+            total *= roundScore;
+
+        }
+        return total;
+    }
+    public static int day62(ArrayList<String> array){
+        ArrayList<Integer>  time = new ArrayList<>();
+        ArrayList<Integer> distance = new ArrayList<>();
+
+        int total = 0;
+        String times = "";
+        String distances = "";
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(array.get(0));
+
+        while(matcher.find()){
+            String temp = matcher.group();
+            System.out.println(temp);
+            times += temp;
+        }
+        System.out.println(times);
+
+        Matcher matcher1 = pattern.matcher(array.get(1));
+
+        while(matcher1.find()){
+            distances += matcher1.group();
+        }
+        System.out.println(distances);
+
+        Long time1 = Long.parseLong(times);
+        Long distance1 = Long.parseLong(distances);
+
+        System.out.println(time1);
+        System.out.println(distance1);
+
+        for(long i = 0; i < time1; i++){
+            Long charge = i;
+            Long timeToTravel = time1-i;
+
+            if((charge * timeToTravel) > distance1){
+
+                total +=1;
+            }
+        }
+        return total;
     }
 }
